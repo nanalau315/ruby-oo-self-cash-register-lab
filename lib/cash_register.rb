7e6137598a82308d12e3448cd8a6a61756259605
Nana Lau <nanalau315@hotmail.com>
2020-11-18 20:28:19 -0500

    commit tree b0865097d20ee63cd69bf59bba78db57c10a256a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+     attr_accessor :total, :discount, :item
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @item = item
+        
+    end
+
+    def add_item(title, price, quantity=1)
+
+
+    end
+
+end
+
+
+# @total.cash_register_with_discount = @total - @total * 20.0 / 100.0
 
